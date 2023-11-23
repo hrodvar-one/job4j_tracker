99e5ac32acc61bbc7c2d297d17a1908db4a1ed1c
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2023-11-23 08:08:12 +0600

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,34 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book firstBook = new Book("Mobi-Dick, or The Whale", 752);
+        Book secondBook = new Book("Java: The Complete Reference", 1345);
+        Book thirdBook = new Book("Grokking Algorithms", 256);
+        Book fourthBook = new Book("Clean code", 464);
+        Book[] books = new Book[4];
+        books[0] = firstBook;
+        books[1] = secondBook;
+        books[2] = thirdBook;
+        books[3] = fourthBook;
+        for (int index = 0; index < books.length; index++) {
+            Book book = books[index];
+            System.out.println(book.getName() + " - " + book.getNumberOfPage());
+        }
+        Book temp = books[0];
+        books[0] = books[3];
+        books[3] = temp;
+        System.out.println("Replaced the book with index 0 with the book with index 3.");
+        for (int index = 0; index < books.length; index++) {
+            Book book = books[index];
+            System.out.println(book.getName() + " - " + book.getNumberOfPage());
+        }
+        System.out.println("Indexes of books with name Clean Code :");
+        for (int index = 0; index < books.length; index++) {
+            Book book = books[index];
+            if (book.getName().equals("Clean code")) {
+                System.out.println(index);
+            }
+        }
+    }
+}
