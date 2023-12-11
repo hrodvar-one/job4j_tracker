493243f94aa649143a99f5413fee4c875f06290f
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2023-12-11 13:02:39 +0600

    5.1. Зачем нужно приведение типов [#310019]

---
+++ src/main/java/ru/job4j/oop/Dog1.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class Dog1 {
+    public static void main(String[] args) {
+        Dog1 polkan = new Dog1();
+        Dog1 sharik = new Dog1();
+        Dog1 zhychka = new Dog1();
+    }
+}
