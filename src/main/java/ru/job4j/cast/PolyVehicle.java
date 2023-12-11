c349a9d7ef9d3654c253635c83dc172a6fa42082
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2023-12-11 15:18:01 +0600

    5.1. Зачем нужно приведение типов [#310019]

---
+++ src/main/java/ru/job4j/cast/PolyVehicle.java
@@ -0,0 +1,26 @@
+package ru.job4j.cast;
+
+public class PolyVehicle {
+    public static void main(String[] args) {
+        Vehicle airplane = new Airplane();
+        Vehicle warPlane = new Airplane();
+        Vehicle train = new Train();
+        Vehicle electricTrain = new Train();
+        Vehicle speedTrain = new Train();
+        Vehicle bus = new Bus();
+        Vehicle shuttleBus = new Bus();
+        Vehicle intercityBus = new Bus();
+        Vehicle doubleDecker = new Bus();
+
+        Vehicle[] vehicles = new Vehicle[]{airplane, warPlane, train,
+                electricTrain,
+                speedTrain,
+                bus,
+                shuttleBus,
+                intercityBus,
+                doubleDecker};
+        for (Vehicle v : vehicles) {
+            v.move();
+        }
+    }
+}
