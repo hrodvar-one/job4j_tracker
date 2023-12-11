d4d9dc9bc72c479d1e0c3aa087be70fac2d8cf6b
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2023-12-11 10:43:33 +0600

    4.1. Элементы интерфейса [#504948 #465792]

---
+++ src/main/java/ru/job4j/polymorph/MainFunc.java
@@ -0,0 +1,13 @@
+package ru.job4j.polymorph;
+
+public class MainFunc implements Func1, Func2 {
+    @Override
+    public double func(double x, double y) {
+        return Func2.super.func(x, y);
+    }
+
+    @Override
+    public void funcMessage() {
+        Func1.super.funcMessage();
+    }
+}
