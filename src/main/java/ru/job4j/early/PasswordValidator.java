1069eec0d7951bcc4a13dacd60a88684a85ce92a
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2023-12-26 10:03:36 +0600

    4. Принципы раннего возврата и охранных выражений [#504875 #468415]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -40,18 +40,18 @@ public class PasswordValidator {
         for (char symbol : password.toCharArray()) {
             if (Character.isUpperCase(symbol)) {
                 hasUpCase = true;
-                continue;
             }
             if (Character.isLowerCase(symbol)) {
                 hasLowCase = true;
-                continue;
             }
             if (Character.isDigit(symbol)) {
                 hasDigit = true;
-                continue;
             }
             if (!Character.isLetterOrDigit(symbol)) {
                 hasSpecial = true;
+            }
+            if (!hasUpCase && !hasLowCase && !hasDigit && !hasSpecial) {
+                break;
             }
         }
         if (!hasUpCase) {
