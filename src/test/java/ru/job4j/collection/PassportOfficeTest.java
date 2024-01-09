07c0b04d47a575ea448ec530fe0b848dc32fbda6
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2024-01-09 07:21:20 +0600

    1. Паспорт и жители [#212656]

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -11,4 +11,13 @@ class PassportOfficeTest {
         office.add(citizen);
         assertThat(office.get(citizen.getPassport())).isEqualTo(citizen);
     }
+
+    @Test
+    public void whenTestNotAddMethod() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen2 = new Citizen("2f44a", "Aleksandr Kobelskiy");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.add(citizen2)).isFalse();
+    }
 }
