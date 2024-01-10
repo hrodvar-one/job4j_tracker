56fc4c5ba227886f587dd6d9cccd6b2a8ffbcf06
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2024-01-10 06:34:28 +0600

    0. Map, HashMap [#212652 #471480]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> hashMap = new HashMap<>();
         hashMap.put("kobel.san.sanich@gmail.com", "Aleksandr Kobelskiy");
+        hashMap.put("parsentev@yandex.ru", "Petr Arsentev");
+        hashMap.put("omskobl@yandex.ru", "Ivan Sidorov");
+        hashMap.put("omskobl@yandex.ru", "Ivan Petrov");
         for (String s : hashMap.keySet()) {
             System.out.println(s);
         }
