a2390c927f407b547ebe5aa64a80534c42b1c1d5
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2024-01-15 14:52:08 +0600

    0. Сортировка [#10096 #473660]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -8,7 +8,6 @@ public class Item {
     private int id;
     private String name;
     private LocalDateTime created = LocalDateTime.now();
-
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
 
     public Item() {
--- src/main/java/ru/job4j/tracker/ItemDescByName.java
+++ src/main/java/ru/job4j/tracker/ItemDescByName.java
@@ -3,7 +3,6 @@ package ru.job4j.tracker;
 import java.util.Comparator;
 
 public class ItemDescByName implements Comparator<Item> {
-
     @Override
     public int compare(Item left, Item right) {
         return right.getName().compareTo(left.getName());
