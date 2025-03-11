1aeab36d2e6fcc693bf674fc999527cecfead18b
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2025-03-11 13:17:18 +0600

    1. Интеграционное тестирование с Hibernate [#6875 #535568]

--- src/main/resources/db/scripts/006_ddl_create_participates_table.sql
+++ src/main/resources/db/scripts/006_ddl_create_participates_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE participates (
+CREATE TABLE if not exists participates (
    id serial PRIMARY KEY,
    item_id int not null REFERENCES items(id),
    user_id int not null REFERENCES j_user(id),
